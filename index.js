7fca58fd47c84451691561714f6cfd6cee4ccbb8
Artemijs Matusev <matu6ev@gmail.com>
2024-03-02 22:37:24 +0000

    Set up the public folder for static files, and set up the front-end index.ejs page to be rendered on the root endpoint

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Imports
+import express from "express";
+
+const app = express()
+const port = 3000;
+
+//Middleware
+app.use(express.static("public"));//Set up public folder for static files
+
+//ENDPOINTS
+//Root endpoint
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+//Start the app
+app.listen(`${port}`, () => {
+    console.log(`Server running on port ${port}`)
+})
